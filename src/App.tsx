0ed17837ff181b86412ffaee5a80e1bb244f0d7c
salvador <salvadoraita@gmail.com>
2024-07-30 18:25:51 -0400

    20240730 18:25

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Button } from "@mui/material"
+import styles from "./App.module.css"
 
 function App() {
 
   return (
-    <>
-      <h1>Buscador de Clima</h1>
-    </>
+    <section>
+      <h1 className={styles.title}>Buscador de Clima</h1>
+
+      <div className={styles.container}>
+        <p>1</p>
+        <p>2</p>
+      </div>
+    </section>
   )
 }
 
